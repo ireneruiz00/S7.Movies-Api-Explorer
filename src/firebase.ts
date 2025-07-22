aef5e991348f44d35b110086c8d43d4933aa6028
ireneruiz00 <ruizsirene2@gmail.com>
2025-07-22 19:10:41 +0200

    Removed submodule and integrated project into root

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDItTJIoEvYI5sxPg-wbfi0q7KiehielEU",
-  authDomain: "s7-movies-api-explorer.firebaseapp.com",
-  projectId: "s7-movies-api-explorer",
-  storageBucket: "s7-movies-api-explorer.firebasestorage.app",
-  messagingSenderId: "292982955814",
-  appId: "1:292982955814:web:38382f16ba7cf7c3876be7",
-  measurementId: "G-YM88WG28F1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
